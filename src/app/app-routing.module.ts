e59c671eca55e4394f9b8a43fa6818a209560650
elisethedev <110583073+elisethedev@users.noreply.github.com>
2023-08-23 16:44:13 -0600

    Update 8/23/23
    added routing
    created components: nav, home, program, contact us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProgramComponent } from './program/program.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", 
+  redirectTo: "Home",
+  pathMatch: 'full',
+  },
+  {path: "Home", 
+  component: HomeComponent
+  },
+  {path: "Program", 
+  component: ProgramComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProgramComponent } from './program/program.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  , HomeComponent, ProgramComponent, NavComponent, ContactUsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
